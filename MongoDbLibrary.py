1b5a824a1beaddd6e646e4fc2d55e5b9c5a3f8ea
Orlando Costa <60602959+orlandoocosta@users.noreply.github.com>
2021-12-07 17:52:37 +0000

    Init commit

---
+++ MongoDbLibrary.py
@@ -0,0 +1,66 @@
+import pymongo
+from robot.api.deco import library, keyword
+import json
+
+
+@library(scope="GLOBAL", version="0.0.1")
+class MongoDbLibrary():
+    """
+    MongoDb Robot Framework library
+    """
+
+    def __init__(self) -> None:
+        self.connection = None
+
+    @keyword("Connecto to MongoDb")
+    def connect_to_mongodb(self, conn_str: str) -> None:
+        """
+        Connect to MongoDb database with connection string.
+
+        Example:
+        | `Connect to MongoDb` | ${conn_str}
+        """
+
+        self.connection = pymongo.MongoClient(
+            conn_str, serverSelectionTimeoutMS=5000)
+
+        try:
+            print("Connecton successfull")
+        except Exception:
+            print("Unable to connect to the server.")
+
+    @keyword("Execute MongoDb Query")
+    def execute_mongodb_query(self, db_name, collection_name, json_query):
+       """
+        Perform mongoDb query and return a list of records containing all query results based on the JSON entered.
+
+        Example:
+        | `Execute MongoDb Query` | ${json_query}
+        """
+
+       db = self.connection.get_database(db_name)
+       collection = db.get_collection(collection_name)
+       return collection.find_one(json_query)
+
+    @keyword("Disconnecto from MongoDb")
+    def diconnect_from_mongodb(self) -> None:
+        """
+        Disconnect from MongoDb database.
+
+        Example:
+        | `Disconnect from MongoDb` |
+        """
+
+        self.connection.close()
+        print("Connection closed.")
+
+
+# Debug
+a = MongoDbLibrary()
+a.connect_to_mongodb(
+    "mongodb://carlsberg:carlsberg@localhost:27017/cx-order-service?readPreference=primary&ssl=false&authsource=admin")
+b = a.execute_mongodb_query("cx-order-service", "order",
+                            {"orderNumber": "120000000"})
+
+print(b)
+a.diconnect_from_mongodb()
